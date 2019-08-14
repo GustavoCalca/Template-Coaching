ee3e8fba6ef2a6dd140116ec91393e7f95237042
Rafael David <rafaeldsaf@live.com>
2019-08-14 14:28:02 -0400

    Subida Inicial

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<!-- Meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="Auditor, Fiscal, Coaching, Concursos, Públicos">
+	<meta name="description" content="Coaching para concursos públicos nas áreas fiscal, tribunal, policial, controle e gestão.">
+	<meta name="author" content="Coaching Concurseiros">
+	<meta name="robots" content="index, follow">
+
+	<!-- Page title -->
+	<title>Coaching Concurseiros</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<!-- Local CSS -->
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+	<!-- Ícone -->
+	<link rel="icon" href="img/icone.png">
+
+	<!-- JQuery -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Bootstrap Popper -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<!-- Bootstrap -->
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<!-- Sweet Alert -->
+	<script type="text/javascript" src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<!-- Font Awesome -->
+	<script type="text/javascript" src="https://kit.fontawesome.com/8a4aee8a86.js"></script>
+	<!-- Local JS -->
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+	<?php
+	include "pages/navbar.html";
+
+	/*
+	include "pages/home.html";
+	*/
+	
+	include "pages/projeto.html";
+	include "pages/inicio-treinamento.html";
+	include "pages/sqi.html";
+	include "pages/cursos.html";
+	include "pages/para-todos.html";
+	include "pages/entrevistas.html";
+	include "pages/aula-gratuita.html";
+	include "pages/equipe.html";
+	
+	include "pages/footer.html";
+	?>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,135 @@
+$(function() {
+	// Set Coach Initial Img
+	setCoachImg();
+
+	// Set Curso/Para Todos 'alt' Img
+	setCursoImg();
+	setParaTodosImg();
+
+	// Check SQI .card discount and if(.disabled)
+	checkCard();
+
+	// COACH Set 'collapse' <button> .active onclick / Flip '▲'
+	$('#equipe .row > .col:not(:last-child) > .card').each(function(e) {
+		let card = this;
+		$('.card-footer button:first-child', card).click(function(e) {
+			if($(this).hasClass('active')) {
+				$(this).removeClass('active');
+				$('.card-body > div:first-child', card).removeClass('toggled');
+				$('.card-footer button.disabled > span', card).removeClass('rotated');
+			} else {
+				$(this).addClass('active');
+				$('.card-body > div:first-child', card).addClass('toggled');
+				$('.card-footer button.disabled > span', card).addClass('rotated');
+			}
+		});
+	});
+
+	// CURSOS Set 'collapse' <button> .active onclick / Flip '▲'
+	$('#cursos .card').each(function(e) {
+		let card = this;
+		$('.card-header .text-right > button:nth-child(3)', card).click(function(e) {
+			if($(this).hasClass('active')) {
+				$(this).removeClass('active');
+				$('.card-body', card).removeClass('toggled');
+				$('.card-header button > span:last-child', card).removeClass('rotated');
+			} else {
+				$('#cursos .card .card-header .active').removeClass('active');
+				$('#cursos .toggled').removeClass('toggled');
+				$('#cursos .rotated').removeClass('rotated');
+				$(this).addClass('active');
+				$('.card-body', card).addClass('toggled');
+				$('.card-header button > span:last-child', card).addClass('rotated');
+			}
+		});
+	});
+
+	// PARA-TODOS Set 'collapse' <button> .active onclick / Flip '▲'
+	$('#para-todos > .container > .row:not(:last-of-type) .card').each(function(e) {
+		let card = this;
+		$('.card-header .text-right > a:last-of-type', card).click(function(e) {
+			if($(this).hasClass('active')) {
+				$(this).removeClass('active');
+				$('.card-body', card).removeClass('toggled');
+				$('.card-header a > span:last-child', card).removeClass('rotated');
+			} else {
+				$('#para-todos > .container > .row:not(:last-of-type) .card .card-header .active').removeClass('active');
+				$('#para-todos .toggled').removeClass('toggled');
+				$('#para-todos .rotated').removeClass('rotated');
+				$(this).addClass('active');
+				$('.card-body', card).addClass('toggled');
+				$('.card-header a > span:last-child', card).addClass('rotated');
+			}
+		});
+	});
+
+	// CURSO set iframe onclick
+	$('#cursos .card > .card-body u').click(function(e) {
+		$('#curso-modal iframe').attr('src', $(this).attr('src'));
+		$('#curso-modal').modal('show');
+	});
+	// CURSO stop video onmodalclose
+	$('#curso-modal').on('hide.bs.modal', function(){
+		$('iframe', this).attr('src', '');
+	});
+});
+
+// COACH Set Img
+function setCoachImg() {
+	let id = 1;
+	$('#equipe .row > .col:not(:last-child) > .card').each(function(e) {
+		$('.card-body > img', this).attr('alt', $('.card-body p > span', this).text());
+		$('.card-body > img', this).attr('src', './img/coaches/img-nome-'+id+'.jpg');
+
+		id += 1;
+	});
+};
+
+// CURSOS Set Img 'alt'
+function setCursoImg() {
+	$('#cursos .container > .row').each(function(e) {
+		$('> div img', this).attr('alt', $('> div > .card > .card-header h2', this).text());
+	});
+};
+
+// PARA TODOS Set Img 'alt'
+function setParaTodosImg() {
+	$('#para-todos .container > .row').each(function(e) {
+		$('> div img', this).attr('alt', $('> div > .card > .card-header h2', this).text());
+	});
+};
+
+// SQI Check diff preço / .disabled state
+function checkCard() {
+	$('#sqi .sqi-planos .disponivel .card').each(function(e) {
+		// Discount
+		if($('.preco-inicial', this).text() != $('.preco-desconto', this).text()) {
+			$('.preco-desconto', this).css('visibility', 'initial');
+			$('.preco-inicial', this).css({
+				'text-decoration': 'line-through',
+				'font-weight': 'initial',
+				color: 'red',
+				opacity: 0.6
+			});
+			$(' > .sale', this).show();
+		} else {
+			$('.preco-desconto', this).css('visibility', 'hidden');
+			$('.preco-inicial', this).css({
+				'text-decoration': 'none',
+				'font-weight': 'bold',
+				color: 'inherit',
+				opacity: 1
+			});
+			$('> .sale', this).hide();
+		}
+
+		// Disabled
+		if($(this).hasClass('disabled')) {
+			$('.card-header > span:first-child > i:last-of-type', this).attr('class', 'fas fa-lock fa-stack-1x fa-inverse');
+			$('.card-footer > button', this).text('Em Breve');
+		} else {
+			$('.card-header > span:first-child > i:last-of-type', this).attr('class', 'fas fa-dollar-sign fa-stack-1x fa-inverse');
+			$('.card-footer > button', this).text('Comprar');
+		}
+	});
+};
